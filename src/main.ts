eaae860f194e64ce420f2cd810cca5466755c8a7
Filip Brecher <filip.brecher@gmail.com>
2025-06-07 12:00:58 +0200

    Add detection and notification of multiple opened tabs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
 import "./style.css";
 import {App} from "./app.ts"
 
+const channel = new BroadcastChannel("infinite-fish");
+
+let isMainTab = true;
+
+channel.onmessage = (e) => {
+    if (e.data === "check-presence") {
+        channel.postMessage("active-tab");
+    } else if (e.data === "active-tab") {
+        isMainTab = false;
+    }
+};
+
+channel.postMessage("check-presence");
+
+setTimeout(() => {
+    if ( !isMainTab) {
+        alert("Another tab is already running this app. Please close other tabs to avoid losing data in your saves.");
+        window.close();
+    }
+}, 500);
+
 
 export const app = new App();
 await app.init();
