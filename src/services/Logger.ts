76b7c9f5d0a995e46f675c325c3cf5a0c4a8b106
Filip Brecher <filip.brecher@gmail.com>
2025-06-17 00:57:22 +0200

    Update logger and add emojis to it

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -1,25 +1,31 @@
 import {Utils} from "./Utils";
 
 type LogLevel = "info" | "warning" | "error";
-type LogType = "db" | string;
+type LogType = "db" | "settings" | "state_service" | "workspace_service" | "sidebar" | "workspace" | "instance" | "view" | string;
 
 // todo - make an array of structured log messages, which isn't displayed, but can be copies (for example in settings)
 //        that contains all logs, for debugging purposes (upon error, user can copy (or download a file?) and paste to discord)
 //        this could also be optional (if someone doesn't want it to take additional memory)
 export class Logger {
+    private readonly emojiByType: Map<LogType, string> = new Map([
+        ["db", "🛢️"],
+        ["setting", "⚙️"],
+        ["state_service", "🧠"],
+        ["workspace_service", "🧭"],
+        ["sidebar", "🧾"],
+        ["workspace", "🗂️"],
+        ["instance", "📦"],
+        ["view", "🔍️"],
+    ]);
+
     public log(level: LogLevel, type: LogType, message: any): void {
+        const emoji = this.emojiByType.get(type);
+        const emojiPrefix = emoji ? `${emoji} ` : "";
 
-        switch (type) {
-            case "db":
-                const color = Logger.getColor(level);
-                const time = Utils.getFormattedDatetime();
-                const prefix = `${time} %c[${level.toUpperCase()}] ${type.toUpperCase()}:`;
-                console.log(prefix, `color: ${color}; font-weight: bold;`, message);
-                break;
-
-            default:
-                console.log(`${type}:`, message);
-        }
+        const color = Logger.getColor(level);
+        const time = Utils.getFormattedDatetime();
+        const prefix = `${emojiPrefix}${time} %c[${level.toUpperCase()}] ${type.toUpperCase()}:`;
+        console.log(prefix, `color: ${color}; font-weight: bold;`, message);
     }
 
     private static getColor(level: LogLevel): string {
