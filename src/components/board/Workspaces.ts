feeac469b664c6b9df63d395354262503d33cfc1
Filip Brecher <filip.brecher@gmail.com>
2025-07-04 14:30:15 +0200

    Rework workspace tab visuals

--- src/components/board/Workspaces.ts
+++ src/components/board/Workspaces.ts
@@ -1,14 +1,18 @@
 import "./workspace.css";
 import type {IComponent} from "../IComponent";
 import {app} from "../../main";
+import type {WorkspaceProps} from "../../types/db/schema";
 
 
 export class Workspaces implements IComponent {
     private readonly list: HTMLDivElement;
     private activeWsId: number | undefined;
 
+    private tabs: Map<number, HTMLDivElement> = new Map();
+
     constructor() {
         this.list = document.getElementById("workspaces-list") as HTMLDivElement;
+        this.list.addEventListener("mousedown", this.stopPropagation);
 
         app.state._saveUnloaded.subscribe(this.onSaveUnloaded);
         app.state._workspaceUnloaded.subscribe(this.onWorkspaceUnloaded);
@@ -20,6 +24,7 @@ export class Workspaces implements IComponent {
     private onSaveUnloaded = () => {
         this.list.innerHTML = "";
         this.activeWsId = undefined;
+        this.tabs = new Map();
     }
 
     private onSaveLoaded = () => {
@@ -28,32 +33,46 @@ export class Workspaces implements IComponent {
                 return a.position - b.position;
             })
             .forEach((ws) => {
-                const wsDiv = document.createElement("div") as HTMLDivElement;
-                wsDiv.id = `workspace-tab-${ws.id}`;
-                wsDiv.classList.add("workspace-tab");
-                wsDiv.innerText = ws.name;
+                const wrapper = document.createElement("div") as HTMLDivElement;
+                wrapper.classList.add("tab-wrapper");
 
-                wsDiv.addEventListener("click", this.onClickWorkspace);
+                const dots = document.createElement("div") as HTMLDivElement;
+                dots.classList.add("tab-dots");
+                dots.classList.add("icon");
+                dots.classList.add("mask-icon");
+                dots.classList.add("button-icon");
 
-                this.list.append(wsDiv);
+                const name = document.createElement("span") as HTMLSpanElement;
+                name.classList.add("tab-name");
+                name.innerText = `${ws.name}`;
+
+                // wsDiv.setAttribute("contenteditable", "true");
+
+                wrapper.append(dots, name);
+                wrapper.addEventListener("click", (e: MouseEvent) => {this.onClickWorkspace(e, ws.id)});
+
+                this.list.append(wrapper);
+                this.tabs.set(ws.id, wrapper);
             });
     }
 
-    private onWorkspaceUnloaded = () => {
-        if (this.activeWsId) {
-            this.list.querySelector(`#workspace-tab-${this.activeWsId}`).classList.remove("active");
-        }
-    }
+    private onWorkspaceUnloaded = () => {}
 
-    private onClickWorkspace = (event) => {
-        const id = Number(event.target.id.slice(14));
+    private onClickWorkspace = (e: MouseEvent, id: number) => {
         if (this.activeWsId === id) return;
         app.state.loadWorkspace(id).catch();
-        event.stopPropagation();
+        e.stopPropagation();
     }
 
-    private onWorkspaceLoaded = () => {
-        this.activeWsId = app.state.activeWorkspace?.id;
-        this.list.querySelector(`#workspace-tab-${this.activeWsId}`).classList.add("active");
+    private onWorkspaceLoaded = (ws: WorkspaceProps) => {
+        if (this.activeWsId !== undefined) {
+            this.tabs.get(this.activeWsId).classList.toggle("active", false);
+        }
+        this.activeWsId = ws.id;
+        this.tabs.get(this.activeWsId).classList.toggle("active", true);
+    }
+
+    private stopPropagation = (e: Event) => {
+        e.stopPropagation();
     }
 }
